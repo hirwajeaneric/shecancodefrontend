457f2c60cfd07999ed89eb38f9cb24ba83650991
Rutikanga3 <rutikanga@gmail.com>
2023-05-17 10:21:54 +0300

    home first section design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from './components/Header/index';
+import Firstsection from '../src/components/section/alumni/firsthomesection/firstsection'
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Firstsection />
     </div>
   );
 }
