845b97a50434c0bd89a5fc9f8ec20d2c3e8415c6
Your name <you@example.com>
2023-05-17 07:04:23 +0200

    creation of responsive navbar with dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Header from './components/Header/index';
-
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header/>
     </div>
   );
 }
-
 export default App;
