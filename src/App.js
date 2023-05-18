0ddf5e52ce49db3717ba7718d455f1c769f16c5b
mutuyimana1 <amutuyimana64@gmail.com>
2023-05-18 13:35:06 +0200

    fix error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Header from './components/Header/index';
+import Header from "./components/Header/index";
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./routes/index";
 function App() {
   return (
-    <div className="App">
-    <Header />
-      {/* <Footer /> */}
-
-    </div>
+    <Router>
+      <Routes></Routes>
+    </Router>
   );
 }
 export default App;
