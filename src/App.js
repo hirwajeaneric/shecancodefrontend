d478a91af9bdd3e46eab8fd7ee62a9b44cc38b16
Your name <you@example.com>
2023-05-19 06:54:14 +0200

    addition of new features on navigation bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header/index';
+import Nav from './components/Nav';
 function App() {
   return (
     <div className="App">
-      <Header/>
+  <Nav/>
+  <Header/>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,18 +6,20 @@ let Index = () => {
           <div class="all">
             <img src={Image} alt='scc-logo'/>
           <ul class="navbar">
-            <li><a href="#" class="active">Home</a></li>
-            <li><a href="#">About <i class='bx bx-chevron-down'></i></a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About </a></li>
             <li><a href="#">Contact</a></li>
             <li><a href="#">Program</a></li>
             <li><a href="#">Login</a></li>
           </ul>
-          <div class="menu">
-            <button class="btn">Apply</button>
-             <div class= "bx bx-menu" id="menu-icon"></div>
+          <div class="mymenu">
+            <button class="mybtn">Apply</button>
+             <div class= "bx bx-menu" id="mymenu-icon"></div>
           </div>
           </div>
           </div>
     );
 }
 export default Index;
+
+
