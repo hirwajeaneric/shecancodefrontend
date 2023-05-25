b096f80f01aea50aab36b7f39cb02f78b7150e30
mutuyimana1 <amutuyimana64@gmail.com>
2023-05-25 18:57:48 +0200

    test2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/index";
+import About from "../pages/About";
 const Index = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   );
 };
