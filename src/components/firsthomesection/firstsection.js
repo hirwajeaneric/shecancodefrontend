19f4bafe60239c8b8710cbe5523f3a5e90594038
evening8 <ishimwerichard26@gmail.com>
2023-05-26 17:49:59 +0200

    home page

--- src/components/firsthomesection/firstsection.js
+++ src/components/firsthomesection/firstsection.js
@@ -14,7 +14,7 @@ const Firstsection = () => {
             <p className='par'>Irrespective of your prior coding proficiency, whether you possess limited experience, are self-taught, or feel uncertain,we warmly welcome you to join our esteemed program.
              We provide comprehensive training and guidance, <br></br>empowering individuals to acquire valuable coding skills and pave their path towards professional growth</p><br></br>
             <div className='whole'><div className='designer'><i class='bx bxl-codepen'></i></div><h4>UI/UX Design</h4><div className='frontend'><i class='bx bx-code-block'></i></div><h4>Front-end</h4><div className='backend'><i class='bx bxs-data' ></i></div><h4>Back-end</h4></div><br></br>
-            <button id='btn' className='.button'>Get started</button>
+            <button id='btn' className='button'>Get started</button>
             </div>
             <div className='rigth'>
             </div> 
