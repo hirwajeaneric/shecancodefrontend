cb0fa3d9df5440b1bff44c0b338a69a209428d25
evening8 <ishimwerichard26@gmail.com>
2023-05-26 14:47:53 +0200

    fix first section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,17 @@
 import './Home.css'
 import Nav from '../../components/Nav/index';
-// import Explore from '../../components/Section/alumni/explore-course/course';
+import Explore from '../../components/explore-course/course';
 import Header from '../../components/Header/index';
 import Alumni from '../../components/alumnisection/index';
-import FirstHome from '../../components/Section/alumni/firsthomesection/firstsection';
+// import FirstHome from '../../components/firsthomesection/firstsection';
 
 let Home = () =>{
 return(
 <div>
 <Nav /> 
 <Header />
-<FirstHome />
- {/* <Explore /> */}
+{/* <FirstHome /> */}
+<Explore />
 <Alumni /> 
 </div>
 );
