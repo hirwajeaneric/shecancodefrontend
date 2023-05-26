2c50812903ebeedbb2127eccd3db32d978a1414e
evening8 <ishimwerichard26@gmail.com>
2023-05-26 14:39:33 +0200

    first home section

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,14 +3,14 @@ import Nav from '../../components/Nav/index';
 // import Explore from '../../components/Section/alumni/explore-course/course';
 import Header from '../../components/Header/index';
 import Alumni from '../../components/alumnisection/index';
-// import FirstHome from '../../components/Section/alumni/firsthomesection/firstsection';
+import FirstHome from '../../components/Section/alumni/firsthomesection/firstsection';
 
 let Home = () =>{
 return(
 <div>
 <Nav /> 
 <Header />
-{/* <FirstHome /> */}
+<FirstHome />
  {/* <Explore /> */}
 <Alumni /> 
 </div>
