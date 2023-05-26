704ff35c102bee8ed7eb84bc39f5413197e57f5a
evening8 <ishimwerichard26@gmail.com>
2023-05-26 12:25:05 +0200

    fix deployment

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import './Home.css'
 import Nav from '../../components/Nav/index';
 // import Explore from '../../components/Section/alumni/explore-course/course';
 import Header from '../../components/Header/index';
-import Alumni from '../../components/Section/alumni/alumnisection/index';
+// import Alumni from '../../components/alumnisection/index';
 // import FirstHome from '../../components/Section/alumni/firsthomesection/firstsection';
 
 let Home = () =>{
@@ -12,7 +12,7 @@ return(
 <Header />
 {/* <FirstHome /> */}
  {/* <Explore /> */}
-<Alumni /> 
+{/* <Alumni />  */}
 </div>
 );
 }
