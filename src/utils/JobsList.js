2b65540b7026f9fcd706540d339513ed2bad3b73
hirwajeaneric <hirwajeaneric25@gmail.com>
2023-12-22 13:41:04 +0200

    #Feature: Setting up data

--- src/utils/JobsList.js
+++ src/utils/JobsList.js
@@ -1,5 +1,9 @@
-export const JOB_LIST = [
-    {
+import { postDateCounter } from "./PostDaysCounter";
+import JOB_LISTINGS from './JobList.json';
 
+export const generateJobs = () => {
+    for (const job of JOB_LISTINGS) {
+        job.postedSince = postDateCounter(new Date(job.postTime));
     }
-];
+    return JOB_LISTINGS;
+};
