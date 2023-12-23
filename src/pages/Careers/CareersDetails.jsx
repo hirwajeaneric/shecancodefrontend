d6ceac74efa9c34ccd8808ebc708ba96e003243d
hirwajeaneric <hirwajeaneric25@gmail.com>
2023-12-23 10:18:44 +0200

    #Updates: Career details page
    Done with the descriptions

--- src/pages/Careers/CareersDetails.jsx
+++ src/pages/Careers/CareersDetails.jsx
@@ -7,13 +7,21 @@ import { generateJobs } from '../../utils/JobsList';
 
 const CareersDetails = () => {
   const [job, setJob] = useState({});
-  const [roles, setRoles] = useState([]);
+  const [rolesAndResponsibilities, setRolesAndResponsibilities] = useState({});
+  const [otherResponsibilities, setOtherResponsibilities] = useState({});
+  const [experiences, setExperiences] = useState({});
+  const [otherQualities, setOtherQualities] = useState([]);
   const params = useParams();
 
   useEffect(() => {
     var jobs = generateJobs();
-    setJob(jobs.find(job => job.code === params.code));
-    setRoles(jobs.find(role => role.code === params.code).rolesAndResponsibilities.list);
+    var foundJob = jobs.find(job => job.code === params.code);
+
+    setJob(foundJob);
+    setRolesAndResponsibilities(foundJob.rolesAndResponsibilities);
+    setOtherResponsibilities(foundJob.otherResponsibilities);
+    setExperiences(foundJob.experience);
+    setOtherQualities(foundJob.otherQualities);
   },[params.code])
 
   return (
@@ -21,20 +29,71 @@ const CareersDetails = () => {
       <Nav />
       <Header />
       <div className='w-full flex flex-col justify-center items-center pt-6 md:pt-12 mb-10'>
-        <div className='w-11/12 md:w-10/12 mb-6 md:mb-12'>
+        <div className='w-11/12 md:w-10/12 mb-6 md:mb-12 leading-relaxed'>
           <Link to='/careers' className='text-blue-600'>Back to All Jobs</Link>
-          <h1 className='text-4xl text-left p-0 font-bold'>{job.title}</h1>
-          <h2>{job.location}</h2>
+          
+          <h1 className='text-4xl text-left p-0 font-bold my-3'>{job.title}</h1>
+          <h2 className='mb-7'>{job.location}</h2>
           <hr />
-          <p>{job.organizationSummary}</p>
-          <h2 className='text-xl font-bold'>About the Job</h2>
-          <p>{job.aboutTheJob}</p>
-          <h2 className='text-xl font-bold'>Roles and Responsibility</h2>
-          <ul>
-            {roles && roles.map((role, index) => (
-              <li key={index}>{role}</li>
-            ))}
-          </ul>
+          <p className='my-7'>{job.organizationSummary}</p>
+          
+          <h2 className='text-xl font-bold mb-3'>About the Job</h2>
+          <p className='mb-7'>{job.aboutTheJob}</p>
+          
+
+
+          {/* Roles and Responsibilities  */}
+          <h2 className='text-xl font-bold mb-3'>Roles and Responsibility</h2>
+          {rolesAndResponsibilities.introduction && <p className='mt-3'>{rolesAndResponsibilities.introduction}</p>}
+          {rolesAndResponsibilities.list && 
+            <ul className='list-disc mb-4 mt-3 pl-9 pb-6 pt-2'>
+              {rolesAndResponsibilities.list.map((role, index) => (
+                <li key={index}>{role}</li>
+              ))}
+            </ul>
+          }
+          
+
+
+          {/* Other Responsibilities  */}
+          {otherResponsibilities && 
+            <h2 className='text-xl font-bold mb-3'>Other Responsibilities</h2>
+          }
+          {otherResponsibilities.introduction && <p className='mt-3'>{otherResponsibilities.introduction}</p>}
+          {otherResponsibilities.list && 
+            <ul className='list-disc mb-4 mt-3 pl-9 pb-6 pt-2'>
+              {otherResponsibilities.list.map((responsibility, index) => (
+                <li key={index}>{responsibility}</li>
+              ))}
+            </ul>
+          }
+          
+          {/* Experience  */}
+          <h2 className='text-xl font-bold'>Experience</h2>
+          {experiences.introduction && <p className='mt-3'>{experiences.introduction}</p>}
+          {experiences.list && 
+            <ul className='list-disc mb-4 mt-3 pl-9 pb-6 pt-2'>
+              {experiences.list.map((experience, index) => (
+                <li key={index}>{experience}</li>
+              ))}
+            </ul>
+          }
+
+
+
+          
+          {/* Other qualities  */}
+          {otherQualities && <h2 className='text-xl font-bold'>Other Qualities</h2>}
+          {otherQualities && 
+            <ul className='list-disc mb-4 mt-3 pl-9 pb-6 pt-2'>
+              {otherQualities.map((quality, index) => (
+                <li key={index}>{quality}</li>
+              ))}
+            </ul>
+          }
+
+
+          
         </div>
       </div>
       <Footer />
--- src/pages/Careers/JobCard.jsx
+++ src/pages/Careers/JobCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const JobCard = ({job}) => {
     return (
-        <Link to={`/job/${job.code}`} className='flex justify-between shadow-md items-center p-6 hover:bg-slate-100 mt-7'>
+        <Link to={`/job/${job.code}`} className='flex justify-between shadow-lg items-center p-6 bg-slate-50 hover:bg-slate-100 mt-7'>
             <div className=''>
                 <h3 className='text-lg text-blue-500 pt-0'>{job.title}</h3>
                 <span className='text-sm text-gray-400'>{job.postedSince}</span>
